7731846e687bf08cfba978d8cf8c6e23e9e381a4
Dr_Huang_MHT <59129019+drHuangMHT@users.noreply.github.com>
2020-12-06 14:43:09 +0800

    Feat:调整文件结构

--- src/main/java/xyz/icedtech/advagri/item_properties/FIRECRACKER.java
+++ src/main/java/xyz/icedtech/advagri/item_properties/FIRECRACKER.java
@@ -1,10 +1,9 @@
-package xyz.icedtech.advagri.item;
+package xyz.icedtech.advagri.item_properties;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.TntEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -13,15 +12,13 @@ import net.minecraft.world.World;
 
 public class FIRECRACKER extends Item {
     public FIRECRACKER(Settings settings ) {
-        super(settings .maxCount(64) .group(ItemGroup.COMBAT));
+        super(settings .maxCount(64));
     }
 
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
         if (hand == Hand.MAIN_HAND) {
             //Set a TNT, located in player's position
             TntEntity tntEntity = EntityType.TNT.create(world);
-            assert tntEntity != null;
-            tntEntity.setFuse(40);
             tntEntity.setPos(playerEntity.getX(), playerEntity.getY(), playerEntity.getZ());
             //Summon the TNT
             world.spawnEntity(tntEntity);
