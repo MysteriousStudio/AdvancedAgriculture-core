c37b3455fa071932b10cf5d197fd87a50a8e8f5b
BreakIce <suibing112233@outlook.com>
2021-01-18 19:09:52 +0800

    Add some TODO

--- src/main/java/xyz/icedtech/advagri/events/GrassBrokenEvent.java
+++ src/main/java/xyz/icedtech/advagri/events/GrassBrokenEvent.java
@@ -2,7 +2,6 @@ package xyz.icedtech.advagri.events;
 
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.minecraft.block.Blocks;
-import net.minecraft.client.render.BlockBreakingInfo;
 import net.minecraft.entity.ItemEntity;
 
 import java.util.Random;
@@ -22,8 +21,15 @@ public class GrassBreakEvent implements EventInterface {
     @Override
     public void Init() {
 
+        //Todo: 1/18/21 1:01 PM 
+        // User: suibing
+        // Aims: For getting bamboo from grass, only can take the bamboo from BREAKING grass by hand. Not any plan to add some way to get bamboo by using water to get the bamboo.
+        // Desc: The new way to get bamboo.
+        // Stat: True
+        
         PlayerBlockBreakEvents.AFTER.register((world, player, pos, state, entity) -> {
             if (state.getBlock() == Blocks.GRASS || state.getBlock() == Blocks.TALL_GRASS) {
+                //ADD ANY EVENTS ABOUT BREAKING GRASS
                 if (new Random().nextInt(50) == 0) {
                     new ItemEntity(world, pos.getX(), pos.getY(), pos.getZ()).dropItem(Blocks.BAMBOO);
                 }
