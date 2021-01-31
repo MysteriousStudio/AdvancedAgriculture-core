86a4d7aef146355873a1c9ac880abc302ee6cbd8
BreakIce <suibing112233@outlook.com>
2021-01-31 17:33:26 +0800

    [update]删除所有文件的Git cached，改用新的commit消息规范。

--- src/main/java/xyz/icedtech/advagri/ModAta.java
+++
@@ -1,6 +0,0 @@
-package xyz.icedtech.advagri;
-
-public class ModAta {
-    public static final String ModID = "adv_agri_core";
-    public static final String ModEntityNamespace = "adv_agri_core_entity";
-}
