710c5ba4dd20046da44576f900534f867586f5e5
BreakIce <suibing112233@outlook.com>
2021-02-05 15:32:09 +0800

    [feat]:新增部分可锻造的金属块，锻造金属时会播放音效了

--- src/main/java/xyz/icedtech/advagri/item/ForgingHammer.java
+++ src/main/java/xyz/icedtech/advagri/item/ForgingHammer.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.ItemEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import xyz.icedtech.advagri.block.AdvAgriBlocks;
@@ -36,6 +37,9 @@ public class ForgingHammer extends Item {
 
 
         if (matchMetalBlock(block)) {
+
+            context.getPlayer().playSound(SoundEvents.BLOCK_ANVIL_LAND, 1F, 1F);
+
             if (!blockArrayList.containsKey(context.getBlockPos())) {//不存在但是是可敲打方块
                 blockArrayList.put(context.getBlockPos(), 1);
                 return ActionResult.SUCCESS;
@@ -44,6 +48,7 @@ public class ForgingHammer extends Item {
                 if (matchMetalBlock(block)) {
                     new ItemEntity(context.getWorld(), blockPos.getX(), blockPos.getY(), blockPos.getZ()).dropStack(dropPlate(block));
                     context.getWorld().breakBlock(blockPos, false);
+                    context.getPlayer().playSound(SoundEvents.BLOCK_ANVIL_DESTROY, 1F, 1F);
                 }
                 return ActionResult.SUCCESS;
             } else {//敲打累增
@@ -89,6 +94,18 @@ public class ForgingHammer extends Item {
             return new ItemStack(AdvAgriItems.NICKEL_PLATE, 9);
         } else if (Objects.equals(advAgriBlocks.ZINC_BLOCK, block)) {
             return new ItemStack(AdvAgriItems.ZINC_PLATE, 9);
+        } else if (Objects.equals(advAgriBlocks.STEEL_BLOCK, block)) {
+            return new ItemStack(AdvAgriItems.STEEL_PLATE, 9);
+        } else if (Objects.equals(advAgriBlocks.TUNGSTEN_BLOCK, block)) {
+            return new ItemStack(AdvAgriItems.TUNGSTEN_PLATE, 9);
+        } else if (Objects.equals(advAgriBlocks.TITANIUM_BLOCK, block)) {
+            return new ItemStack(AdvAgriItems.TITANIUM_PLATE, 9);
+        } else if (Objects.equals(advAgriBlocks.TIN_BLOCK, block)) {
+            return new ItemStack(AdvAgriItems.TITANIUM_PLATE, 9);
+        } else if (Objects.equals(Blocks.IRON_BLOCK, block)) {
+            return new ItemStack(AdvAgriItems.IRON_PLATE, 9);
+        } else if (Objects.equals(Blocks.GOLD_BLOCK, block)) {
+            return new ItemStack(AdvAgriItems.GOLD_PLATE, 9);
         } else {
             throw new IllegalStateException("Unexpected value: " + block.toString());
         }
